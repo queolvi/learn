6f2f7aa7dc48a48747ee9b06de25c15581c849a8
queolvi <korpachyovdmitriy@gmail.com>
2025-06-08 15:44:09 +0300

    1.2/2

--- stl/1_week/auto.cpp
+++ stl/1_week/auto.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <stdexcept>
+#include <cmath>
 class point_ {
   protected:
-  auto x = 0.0, y = 0.0;
+  double x = 0.0, y = 0.0;
 };
 
 class base_ {
@@ -22,19 +23,27 @@ class tools : protected base_, protected point_ {
     }
   };
 
-  void calc_dist(point_& a_, point_& b);
+  double calc_dist(point_& a_, point_& b);
   void dump(std::ostream& os);
 
   
 };  
   
-void tools::calc_dist(point_& a_, point_& b) {
+double tools::calc_dist(point_& a_, point_& b) {
+  using::point_;
+
+  double res = std::sqrt( std::pow((b_.x - a_.x), 2) + std::pow((b_.y - a_.y), 2));
+  return res;
 
 }
 void tools::dump(std::ostream& os) {
-  for(auto x : point_ ) {
-    os << point_
+  point_* p_ = new point_[size_f_base];
+
+  for(auto x : p_ ) {
+    os << p_;
+    ++p_;
   }
+delete [] p_;
 }
 
 
@@ -43,12 +52,14 @@ int main () {
 
 const size_t size_f_base = 3; 
 point_ arr_points* = new point_[size_f_base];
+
 auto choice_{false};
 
-if(choice == true) {
+if(choice_ == true) {
+
 
 }
 
-
+  
   return 0;
 }

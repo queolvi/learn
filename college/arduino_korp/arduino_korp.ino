04709161df555680e09f9728ff5de336eead1d38
thekorp <146642604+queolvi@users.noreply.github.com>
2025-06-11 10:56:45 +0300

    Update arduino_korp.ino

--- college/arduino_korp/arduino_korp.ino
+++ college/arduino_korp/arduino_korp.ino
@@ -1,60 +1,79 @@
 #include "Arduino.h"
 #include "ezBuzzer.h"
-
-class user {};
-class game {};
-
-int TopTime[3]{0, 0, 0};
-int res_counts = 0;
-int level = 0;
-int milliseconds = 0;
-const int yellow_LED = 0, red_LED = 0,
-blue_LED = 0, green_LED = 0, orange_LED = 0;
-
-const int buttonPin_first = 0, buttonPin_second = 0,
-buttonPin_third = 0, buttonPin_fourth = 0,
-buttonPin_fifth = 0;
-ezBuzzer piezo(0);
-
-
-void game_launch();
-void set_level();
-void low_level();
-void medium_level();
-void hard_level();
-void light_up_LED();
-void sound_reaction();
-void light_up_LEDS();
-void press_buttons();
-void setup();
-void loop();
-
-
-
-void game_launch() {
-
-  
-  Serial.println("Enter level for game: (1 - low; 2 - medium; 3 - hard) \n");
-  while(!Serial.available());
-  set_level();
-
-  
-  sound_reaction(); 
-  light_up_LED();
-  
+namespace {
 
 };
-void set_level() {
+ezBuzzer piezo(0);
+
+class reaction_Game {
+  public:
+  
+  #ifdef need_work
+  ezBuzzer piezo(0);
+  #endif
+
+  private:
+  int TopTime[3]{0, 0, 0};
+  int res_counts = 0;
+  int level = 0;
+  int milliseconds = 0;
+  #ifdef idk what incapsulation must be with LEDS
+  
+  #endif
+
+  public:
+  const int buttonPin_first = 0, buttonPin_second = 0,
+  buttonPin_third = 0, buttonPin_fourth = 0,
+  buttonPin_fifth = 0;
+
+  const int yellow_LED = 0, red_LED = 0,
+  blue_LED = 0, green_LED = 0, orange_LED = 0;
+  public:
+  void low_level(); 
+  void medium_level();
+  void hard_level();
+  void set_level();
+  void game_launch();
+  
+  const int get_yellow_LED(const int& yellow_LED) const;
+  const int get_red_LED(const int& red_LED) const;
+  const int get_blue_LED(const int& blue_LED) const;
+  const int get_green_LED(const int& green_LED) const;
+  const int get_orange_LED(const int& orange_LED) const;
+
+  public:
+  virtual ~reaction_Game(){};
+
+
+  private:
+  void light_up_LED();
+  void sound_reaction();
+  void light_up_LEDS();
+  void press_buttons();
+
+};
+
+//public
+const int reaction_Game::get_yellow_LED( const int& yellow_LED) const { return *this; }
+const int reaction_Game::get_red_LED( const int& red_LED) const { return *this; }
+const int reaction_Game::get_blue_LED( const int& blue_LED) const { return *this; }
+const int reaction_Game::get_green_LED(const int& green_LED) const { return *this; }
+const int reaction_Game::get_orange_LED( const int& orange_LED) const { return *this; }
+
+void reaction_Game::low_level() {level = 1; milliseconds = 400; }
+void reaction_Game::medium_level() {level = 2; milliseconds = 300; }
+void reaction_Game::hard_level() { level = 3; milliseconds = 250; }
+
+void reaction_Game::set_level() {
   Serial.println("Enter game level (1 - low, 2 - medium, 3 - hard || yellow - 1, red - 2, blue - 3) ");
 
+  digitalWrite(get_yellow_LED(), HIGH);
+  digitalWrite(get_yellow_LED(), HIGH);
+  digitalWrite(get_blue_LED(), HIGH);
 
-  digitalWrite(yellow_LED, HIGH);
-  digitalWrite(red_LED, HIGH);
-  digitalWrite(blue_LED, HIGH);
-
-  digitalWrite(yellow_LED, LOW);
-  digitalWrite(red_LED, LOW);
-  digitalWrite(blue_LED, LOW);
+  digitalWrite(get_yellow_LED(), LOW);
+  digitalWrite(get_yellow_LED(), LOW);
+  digitalWrite(get_blue_LED(), LOW);
 
   if (digitalRead(buttonPin_first)) { low_level(); } 
   else if (digitalRead(buttonPin_second)) { medium_level(); }
@@ -64,12 +83,22 @@ void set_level() {
   }
 
 }
-void low_level() {level = 1; milliseconds = 400; }
-void medium_level() {level = 2; milliseconds = 300; }
-void hard_level() { level = 3; milliseconds = 250; }
+// end
 
-void light_up_LED() {
-   int random_launch[5]{ yellow_LED, red_LED, blue_LED, green_LED, orange_LED};
+//private
+
+void reaction_Game::game_launch() {  
+  Serial.println("Enter level for game: (1 - low; 2 - medium; 3 - hard) \n");
+  while(!Serial.available());
+  set_level();
+
+  sound_reaction(); 
+  light_up_LED();
+};
+
+void reaction_Game::light_up_LED() {
+   int random_launch[5]{0, 0, 0, 0, 0};
+   int 
    int button_enter[5] { buttonPin_first, buttonPin_second, buttonPin_third, buttonPin_fourth, buttonPin_fifth };
    
   for(int i = 1; i < 2; i++) {
@@ -80,8 +109,6 @@ void light_up_LED() {
     delay(milliseconds);
     digitalWrite(random_launch[i], LOW);
     
-
-
     for(int j = 0; j < 5; ++j) {
       if(button_enter[j] == random_launch[i]){
         unsigned long buttonPressTime = 0;
@@ -128,7 +155,7 @@ void light_up_LED() {
 
 }
 
-void sound_reaction() {
+void reaction_Game::sound_reaction() {
   int ms = milliseconds;
   piezo.stop();
   piezo.beep(ms);
@@ -136,25 +163,25 @@ void sound_reaction() {
   piezo.stop();
 }
 
-void light_up_LEDS() {
+void reaction_Game::light_up_LEDS() {
   
   for(int i = 1; i <= 3; i++) {  
-  digitalWrite(yellow_LED, HIGH);
-  digitalWrite(red_LED, HIGH);
-  digitalWrite(blue_LED, HIGH);
-  digitalWrite(green_LED, HIGH);
-  digitalWrite(orange_LED, HIGH);
+  digitalWrite(get_yellow_LED(), HIGH);
+  digitalWrite(get_red_LED(), HIGH);
+  digitalWrite(get_blue_LED(), HIGH);
+  digitalWrite(get_green_LED(), HIGH);
+  digitalWrite(get_orange_LED(), HIGH);
   
-  digitalWrite(yellow_LED, LOW);
-  digitalWrite(red_LED, LOW);
-  digitalWrite(blue_LED, LOW);
-  digitalWrite(green_LED, LOW);
-  digitalWrite(orange_LED, LOW);
+  digitalWrite(get_yellow_LED(), LOW);
+  digitalWrite(get_red_LED(), LOW);
+  digitalWrite(get_blue_LED(), LOW);
+  digitalWrite(get_green_LED(), LOW);
+  digitalWrite(get_orange_LED(), LOW);
   }
 
 }
 
-void press_buttons() {
+void reaction_Game::press_buttons() {
 int array_buttons[5]{buttonPin_first, buttonPin_second,
                     buttonPin_third, buttonPin_fourth,
                     buttonPin_fifth};
@@ -170,23 +197,20 @@ void setup() {
 }
 
 void loop() {
-  game_launch();
+  using::reaction_Game; 
+  reaction_Game Game{};
+  Game.game_launch();
   
-  pinMode(yellow_LED, OUTPUT);
-  pinMode(red_LED, OUTPUT);
-  pinMode(blue_LED, OUTPUT);
-  pinMode(green_LED, OUTPUT);
-  pinMode(orange_LED, OUTPUT);
+  pinMode(get_yellow_LED(), OUTPUT);
+  pinMode(get_red_LED(), OUTPUT);
+  pinMode(get_blue_LED(), OUTPUT);
+  pinMode(get_green_LED(), OUTPUT);
+  pinMode(get_orange_LED(), OUTPUT);
   
-  digitalWrite(yellow_LED, LOW);
-  digitalWrite(red_LED, LOW);  
-  digitalWrite(blue_LED, LOW);
-  digitalWrite(green_LED, LOW); 
-  digitalWrite(orange_LED, LOW);
+  digitalWrite(get_yellow_LED(), LOW);
+  digitalWrite(get_red_LED(), LOW);  
+  digitalWrite(get_blue_LED(), LOW);
+  digitalWrite(get_green_LED(), LOW); 
+  digitalWrite(get_orange_LED(), LOW);
   
-
-
-
-
-
 }

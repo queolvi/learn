b3446b0155a3bb2d918b2d80b8cac5646760e967
queolvi <korpachyovdmitriy@gmail.com>
2025-08-18 18:47:43 +0300

    wop on algos (№6 on gradient methods)

--- learn_cpp/algos/2_algos_optimization/gradient_methods/5_biconjugate_gradient_method/biconjuatged_gradient_method.cpp
+++ learn_cpp/algos/2_algos_optimization/gradient_methods/5_biconjugate_gradient_method/biconjuatged_gradient_method.cpp
@@ -1,49 +1,13 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <stdexcept>
-class Matrix;
-
+#include "matrix_example.hpp"
 int main () {
 
+Matrix mat(2, 2);
+mat(0,0) = 1;  mat(0,1) = 2;
+mat(1,0) = 3;  mat(1,1) = 4;
+  std::vector<double> vec = {1,2};
+  std::vector<double> res = mat.mult_vec(vec);
+  std::cout << "Res: [" << res[0] << ", " << res[1] << "]\n";
   return 0;
 }
 
-class Matrix {
-  private:
-    std::vector <std::vector<double> data>;
-    size_t rows, cols;
-  public:
-    Matrix(size_t m, size_t n) : rows(m), cols(n), data(m, std::vector<double>(n, 0)) {}
-    Matrix(const std::vector<std::vector<double>>& mat) : data(mat), rows(mat.size()), cols(mat.empty() ? 0 : mat[0].size()) {}
-    double& operator()(size_t i, size_t j) {return data[i][j]; }
-    const double& operator() (size_t i, size_t j) const {return data[i][j]; }
-    size_t nrows() const {  return rows;  } 
-    size_t ncols() const {  return cols;  }
-    std::vector<double> mult_vec(const std::vector<double>& vec) const {
-      if (vec.size() != cols) { throw std::invalid_argument("Matrix and vector dimensions mismatch \n";)}
-      std::vector<double> res (rows, 0.0);
-      for(size_t i = 0; i < rows; ++i) {
-        for(size_t j = 0; j < cols; ++i) {
-          res[i][j] += data[i][j] * vec[j];
-        }
-      }
-      return res;
-    }
-  Matrix transpose() const {
-    Matrix res(cols, rows);
-    for(size_t i = 0; i < rows; ++i) {
-      for(size_t j = 0; j < cols; ++j) {
-        res (i, j) = data[i][j];
-      }
-    }
-    return res;
-  }
-static double dot_product(const std::vector<double>& a, const std::vector<double>& b) {
-  if (a.size() != b.size()) {throw std::invalid_argument("Vecs must have same size for foo");}
-  double res = 0;
-  for(size_t i = 0; i < a.size(); ++i) {
-    res += a[i] * b[i];
-  }
-  return res;
-}
+
---
+++ learn_cpp/algos/2_algos_optimization/gradient_methods/5_biconjugate_gradient_method/matrix_example.hpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <stdexcept>
+
+class Matrix {
+  private:
+    std::vector <std::vector<double>> data;
+    size_t rows, cols;
+  public:
+    Matrix(size_t m, size_t n) : rows(m), cols(n), data(m, std::vector<double>(n, 0)) {}
+    Matrix(const std::vector<std::vector<double>>& mat) : data(mat), rows(mat.size()), cols(mat.empty() ? 0 : mat[0].size()) {}
+    double& operator()(size_t i, size_t j) {return data[i][j]; }
+    const double& operator() (size_t i, size_t j) const {return data[i][j]; }
+    size_t nrows() const {  return rows;  } 
+    size_t ncols() const {  return cols;  }
+    std::vector<double> mult_vec(const std::vector<double>& vec) const {
+      if (vec.size() != cols) { throw std::invalid_argument("Matrix and vector dimensions mismatch \n");}
+      std::vector<double> res (rows, 0.0);
+      for(size_t i = 0; i < rows; ++i) {
+        for(size_t j = 0; j < cols; ++j) {
+          res[i] += data[i][j] * vec[j];
+        }
+      }
+      return res;
+    }
+  Matrix transpose() const {
+    Matrix res(cols, rows);
+    for(size_t i = 0; i < rows; ++i) {
+      for(size_t j = 0; j < cols; ++j) {
+        res (j, i) = data[i][j];
+      }
+    }
+    return res;
+  }
+static double dot_product(const std::vector<double>& a, const std::vector<double>& b) {
+  if (a.size() != b.size()) {throw std::invalid_argument("Vecs must have same size for foo");}
+  double res = 0;
+  for(size_t i = 0; i < a.size(); ++i) {
+    res += a[i] * b[i];
+  }
+  return res;
+}
+};
+
